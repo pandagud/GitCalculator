b52deb3ccca11beba02167f0fa1e2f9823682b86
Jacob Lindvald Pedersen <au520517@uni.au.dk>
2018-02-01 11:55:29 +0100

    Min ændringer er bedst!

--- Calculator.Test.Unit/CalculatorUnitTests.cs
+++ Calculator.Test.Unit/CalculatorUnitTests.cs
@@ -79,7 +79,7 @@ namespace Calculator.Test.Unit
         public void Divid_with_Zero(double x, double y)
         {
             
-            Assert.IsTrue(double.IsInfinity(x/y));
+            Assert.IsTrue(!double.IsInfinity(x/y));
             
         }
     }
